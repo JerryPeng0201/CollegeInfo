d001945812a748e95b9715ccbc9141aba1b81eb0
ZXX <zhang-xuxin@hotmail.com>
2018-07-23 11:20:36 -0400

    post-modal bug fix

--- public/javascripts/modalReload.js
+++ public/javascripts/modalReload.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("button.detail-button").click(function(event) {
     const pname = event.currentTarget.attributes[1].nodeValue;
-    const pdes = event.currentTarget.attributes[1].nodeValue;
+    const pdes = event.currentTarget.attributes[2].nodeValue;
     $("#exampleModalLong .modal-title").text(pname)
     $("#exampleModalLong .modal-body").text(pdes)
     $("#exampleModalLong").modal("show"); 
