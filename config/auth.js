902d5972261eb3efec4a791580af03b8ac78e58a
Jialin Zhou <jialinzh@brandeis.edu>
2018-07-30 15:55:26 -0400

    Fixed google auth callbackURL

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'googleAuth' : {
         'clientID': '713887466308-v156ae883cpu2g6m431mesbkq4bchuuk.apps.googleusercontent.com',
         'clientSecret'  : 'MJfo69bZi4ftJON2Wdr42wP1',
-        'callbackURL'   : 'http://localhost:7300/BrandeisHome/authorized'
+        'callbackURL'   : '/BrandeisHome/authorized'
     }
 
 };
